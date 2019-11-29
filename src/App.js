69dc6ee6210533b9eb5cf5d8bacc3c2d1e7ca31e
natashaw <natashawijoyo@gmail.com>
2019-11-29 19:30:15 +0100

    intro tween

--- src/App.js
+++ src/App.js
@@ -1,26 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {Controller, Scene} from 'react-scrollmagic';
+import {Tween, Timeline, Controls} from 'react-gsap';
+import styled from 'styled-components';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './assets/img/logo.png';
+import one from './assets/img/one.png';
+import two from './assets/img/two.png';
+
+
+const TweenStyled = styled.div`
+    .section {
+        height: 100vh;
+    }
+
+    .mario {
+        transition: width 0.3s ease-out;
+        width: 200px;
+        height: 100px;
+        top: 500px;
+        position: absolute;
+        background-image: url(${one});
+        background-size: 200px
+
+        &.hello{
+            left: 400px;
+            position: absolute;
+            width: 200px;
+            height: 200px;
+            background-image: url(${two});
+            background-size: 200px
+        }
+    }
+`;
+
+const TweenComponent = () => (
+    <TweenStyled>
+        <div className="section" />
+        <div id="trigger" />
+        <Controller>
+            <Scene
+                triggerElemet="#trigger"
+                duration={150}
+                offset={50}
+                indicators={true}
+            >
+            {(progress) => (
+                <Timeline
+                    target={
+                        <div className="mario"></div>
+                    }
+                    repeat={0}
+                    duration={10}
+                    totalProgress={progress}
+                    paused
+                >
+                    <Tween
+                        from={{ x: '0px'}}
+                        to={{ x: '400px'}}
+                        ease="Linear.easeOut"
+                    />
+                    <Tween
+                        to={{ opacity: 0 }}
+                        ease="Strong.easeOut"
+                        delay={0.1}
+                    />
+                </Timeline>
+            )}
+            </Scene>
+            <div id="trigger1" />
+            <Scene
+                triggerElemet="#trigger1"
+                offset={-280}
+                classToggle={['.mario', 'hello']}
+                indicators={true}
+            >
+                <Tween
+                    from={{ opacity: 0 }}
+                    to={{ opacity: 1 }}
+                >
+                    <div className="mario"></div>
+                </Tween>
+            </Scene>
+        </Controller>
+        <div className="section" />
+
+    </TweenStyled>
+);
+
+class App extends Component {
+    render() {
+        return (
+            <TweenComponent />
+        );
+    }
 }
 
 export default App;

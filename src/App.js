5d82623d585fc9f3feb9ec6cab745e906212bc52
natashaw <natashawijoyo@gmail.com>
2020-02-13 11:12:14 +0100

    added responsive css and replaced assets

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
-import {Tween, Timeline, Controls} from 'react-gsap';
-import Plx from 'react-plx';
+import {Tween} from 'react-gsap';
+import {useMediaQuery} from 'react-responsive';
+
 import './App.scss';
 
-import { Sidebar } from './assets/js/Sidebar';
+import {Sidebar} from './assets/js/Sidebar';
+import {SidebarMobile} from './assets/js/SidebarMobile';
+import Content from './assets/js/Content';
 
-import welcome from './assets/img/welcome.gif';
+import welcome from './assets/img/illustration1.svg';
 
 const Homepage = () => (<Container fluid>
+    {/*TODO: implement responsive CSS classes (min screen width)*/}
     <Row id="header">
         {/* logo */}
         <Col id="containers">
@@ -33,14 +37,15 @@ const Homepage = () => (<Container fluid>
                     opacity: 1,
                     y: 0
                 }} duration={1}>
-                <div className="welcomeGif">
+                {/* TODO: remove inline styling and set width-height to fit screen size */}
+                <div className="welcomeGif" style={{ width: '100vw', height: '70vh' }}>
                     <img src={welcome}/>
                 </div>
             </Tween>
         </Col>
     </Row>
     <Row>
-        <Col id="containers">
+        <Col id="containers" style={{ marginBottom: '30vh' }}>
             <Tween from={{
                     opacity: 0,
                     y: 0
@@ -53,17 +58,39 @@ const Homepage = () => (<Container fluid>
                 yoyo={true}
             >
                 <div className="scrollButton"/>
+                {/*TODO: setup anchor scroll upon clicking scrollButton*/}
             </Tween>
         </Col>
     </Row>
+    {/* beginning of portfolio content feed */}
+    <Row>
+        <Col id="containers" style={{ justifyContent: 'center', marginRight: '1%'}}>
+            <Content/>
+        </Col>
+    </Row>
 </Container>);
+
+const Desktop = ({ children }) => {
+    const isDesktop = useMediaQuery({ minWidth: 1200 })
+    return isDesktop ? children : null
+}
+
+const Mobile = ({ children }) => {
+  const isMobile = useMediaQuery({ maxWidth: 1199 })
+  return isMobile ? children : null
+}
 
 class App extends Component {
     render() {
         return (
             <div>
                 <Homepage/>
-                <Sidebar/>
+                <Desktop>
+                    <Sidebar/>
+                </Desktop>
+                <Mobile>
+                    <SidebarMobile/>
+                </Mobile>
             </div>
         );
     }
